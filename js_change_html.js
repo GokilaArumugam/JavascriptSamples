d39f3b3cd0be1f38c42638ef1288aee29415b82d
GokilaArumugam <gokigokila97513@gmail.com>
2023-04-20 09:14:06 +0530

    js_samples

---
+++ js_change_html.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+document.getElementById("demo").innerHTML = "Hello JavaScript!";
+
+function changeh1()
+{
+    document.getElementById("head1").innerHTML="<a href='#'>Change Heading</a>";
+}
